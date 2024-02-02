aa1c4fb62741ae62ac4a194f516888c874b56f68
Angie Cohen <angiedev2001@gmail.com>
2024-02-02 17:07:39 -0300

    Conexion a la base de datos exitosa

---
+++ Backend/database/config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+
+    console.log("Base de datos online, todo ok");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al momento de inicializar la base de datos");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
--- Backend/models/server.js
+++ Backend/models/server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 
-const {createServer} = require('http');
+const { createServer } = require("http");
+
+const { dbConnection } = require("../database/config");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.server = createServer(this.app);
+
+    // Conectar a la base de datos
+    this.conectarDB();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   listen() {
