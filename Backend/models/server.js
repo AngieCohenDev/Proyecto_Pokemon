02a71c90bf929eb426ebcbee80d9000870df0acd
Angie Cohen <angiedev2001@gmail.com>
2024-02-02 16:25:57 -0300

    Inicio del proyecto, creacion del servidor

---
+++ Backend/models/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const {createServer} = require('http');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.server = createServer(this.app);
+  }
+
+  listen() {
+    this.server.listen(this.port, () => {
+      console.log("Servidor corriendo en el puerto ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
